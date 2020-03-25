f5c12321180c8f48afa4bff610681da9d71818c1
Dante Monaldo <dante.monaldo@gmail.com>
2020-03-25 15:42:14 -0700

    Accept command line input

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('hi.')
+const readline = require('readline');
+
+const line = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let stack = [];
+
+line.prompt();
+
+line.on('line', (input) => {
+    if (input.length > 0)
+        stack.push(...input.split(' '));
+    console.log(stack)
+    line.prompt();
+});
+
+// listen for control-c
+line.on('SIGINT', () => {
+    line.close();
+});
