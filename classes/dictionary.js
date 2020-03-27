5ad51b23087a79b52839bfe84b39fd623a07d00b
Dante Monaldo <dante.monaldo@gmail.com>
2020-03-27 15:28:44 -0700

    Use Dictionary.add() to add predefined words

--- classes/dictionary.js
+++ classes/dictionary.js
@@ -1,19 +1,27 @@
 class Dictionary {
     constructor() {
-        this._dictionary = [
-            ['.', (s) => {return s.pop()}],
-            ['.s', (s) => {s.print()}],
-            ['+', (s) => {s.push(s.pop() + s.pop())}],
-            ['-', (s) => {
-                let a = s.pop(), b = s.pop();
-                s.push(b - a);
-            }],
-            ['*', (s) => {s.push(s.pop() * s.pop())}],
-            ['/', (s) => {
-                let a = s.pop(), b = s.pop();
-                s.push(Math.floor(b / a));
-            }],
-        ];
+        this._dictionary = [];
+
+        this.add('.', (s) => {
+            return s.pop()
+        });
+        this.add('.s', (s) => {
+            s.print()
+        });
+        this.add('+', (s) => {
+            s.push(s.pop() + s.pop())
+        });
+        this.add('-', (s) => {
+            let a = s.pop(), b = s.pop();
+            s.push(b - a);
+        });
+        this.add('*', (s) => {
+            s.push(s.pop() * s.pop())
+        });
+        this.add('/', (s) => {
+            let a = s.pop(), b = s.pop();
+            s.push(Math.floor(b / a));
+        });
     }
 
     // add a word and definition to the dictionary
